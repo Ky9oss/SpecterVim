6a519d7ad810cce0afe2f9a4659fada261515b7b
Ky9oss <bb3343956556@163.com>
2025-09-25 18:58:02 +0800

    update

--- lua/plugins/omnisharp.lua
+++ lua/plugins/omnisharp.lua
@@ -16,6 +16,17 @@ return {
     }
     require 'lspconfig'.omnisharp.setup(config)
 
+    vim.keymap.set(
+      "n",
+      "gr",
+      function() require("omnisharp_extended").telescope_lsp_references(require("telescope.themes").get_ivy({ excludeDefinition = true })) end,
+      { noremap = true }
+    )
+    vim.keymap.set("n", "gd", require("omnisharp_extended").telescope_lsp_definition, { noremap = true })
+    vim.keymap.set("n", "<leader>D", function() require("omnisharp_extended").telescope_lsp_references() end,
+      { noremap = true })
+    vim.keymap.set("n", "gi", require("omnisharp_extended").telescope_lsp_implementation, { noremap = true })
+
     -- vim.lsp.config('omnisharp', {
     --   handlers = {
     --     ["textDocument/definition"] = require('omnisharp_extended').definition_handler,
