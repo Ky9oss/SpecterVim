4dcb67aff22432e624a83f68ae78b4950d1f2531
Ky9oss <bb3343956556@163.com>
2026-01-13 18:22:33 +0800

    update lsp

--- lua/plugins/lsp/nvim-lspconfig.lua
+++ lua/plugins/lsp/nvim-lspconfig.lua
@@ -1,10 +1,12 @@
-return {
-  "neovim/nvim-lspconfig",
-  -- ft = { "c", "cpp", "rust", "python", "asm", "lua" },
-  lazy = false,
-  -- config = function()
-  --
-  --
-  -- end
-}
+-- return {
+--   "neovim/nvim-lspconfig",
+--   -- ft = { "c", "cpp", "rust", "python", "asm", "lua" },
+--   lazy = false,
+--   -- config = function()
+--   --
+--   --
+--   -- end
+-- }
+
+return {}
 
