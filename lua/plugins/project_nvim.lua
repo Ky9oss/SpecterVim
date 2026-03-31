09afcd261ace54cf2afaf7211140ba3b010b0c0c
Ky9oss <Ky9oss@163.com>
2026-03-31 23:44:24 +0800

    update

--- lua/plugins/project_nvim.lua
+++ lua/plugins/project_nvim.lua
@@ -1,10 +1,19 @@
 return {
-  "ahmedkhalf/project.nvim",
-  config = function()
-    require("project_nvim").setup({
-      detection_methods = { "pattern", "lsp" },
-      -- cmd: `touch .r00t_here` to mark a project
-      patterns = { ".git", ".gitignore", ".r00t_here", "package.json", ".editorconfig", "Cargo.toml", "*.sln", "CMakeLists.txt" },
-    })
-  end,
+	"ahmedkhalf/project.nvim",
+	config = function()
+		require("project_nvim").setup({
+			detection_methods = { "pattern", "lsp" },
+			-- cmd: `touch .r00t_here` to mark a project
+			patterns = {
+				".r00t_here",
+				".git",
+				".gitignore",
+				"package.json",
+				".editorconfig",
+				"Cargo.toml",
+				"*.sln",
+				"CMakeLists.txt",
+			},
+		})
+	end,
 }
