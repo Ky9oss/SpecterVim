b1c995516e03efbb0721cdbbed16072221751699
Ky9oss <Ky9oss@163.com>
2026-04-08 17:51:06 +0800

    update docs

--- init.lua
+++ init.lua
@@ -65,7 +65,6 @@ vim.opt.shiftwidth = 4
 vim.opt.softtabstop = 4
 vim.opt.expandtab = true
 vim.opt.shada = "'0,<1000,:0,n" .. shada_path
-vim.opt.tags = "tagfiles/**/tags"
 vim.o.autoread = true
 
 -- CRLF to LF
