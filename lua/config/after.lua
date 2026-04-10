b867c48fd5459fb875608ef6354c72ffb3fdedfa
Ky9oss <Ky9oss@163.com>
2026-04-10 20:43:44 +0800

    update

--- lua/config/after.lua
+++ lua/config/after.lua
@@ -3,7 +3,7 @@ local project_root_path = project.get_project_root()
 
 if project_root_path then
   local doc_tags = vim.fn.stdpath("config") .. "/doc/**/tags"
-	vim.opt.tags = { project_root_path .. "/tagfiles/**/tags", project_root_path .. "/ttt/tags" }
+	vim.opt.tags = { project_root_path .. "/tagfiles/**/tags", doc_tags }
 else
 	vim.opt.tags = "tagfiles/**/tags"
 end
